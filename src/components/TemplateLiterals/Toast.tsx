db554c3625cb33c56a4ff8ef7f0a7b7698e43c0e
Akinola <adewoleakinola24@gmail.com>
2023-07-04 17:53:44 +0100

    Position Props

--- src/components/TemplateLiterals/Toast.tsx
+++ src/components/TemplateLiterals/Toast.tsx
@@ -4,6 +4,12 @@
  * "center-bottom" | "Right-center" | "Right-Top" | "Right-Bottom"
  */
 
+type HorizontalPosition= "Right" | "Center" | "Left"
+type VerticalPosition= "Top" | "Center" | "Bottom"
+
+type ToastProps={
+    position:`${HorizontalPosition}-${VerticalPosition}`
+}
 export const Toast=({Position})=>{
     return (
         <div>Toast Notification Position - {Position}</div>
