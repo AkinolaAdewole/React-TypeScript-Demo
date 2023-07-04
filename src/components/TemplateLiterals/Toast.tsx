789d1f09f3eecf6d7a57a0199d584824041adc29
Akinola <adewoleakinola24@gmail.com>
2023-07-04 17:38:57 +0100

    TEMPLATE LITERALS

---
+++ src/components/TemplateLiterals/Toast.tsx
@@ -0,0 +1,11 @@
+/**
+ * Position prop can one of the:
+ * "left-center"| "left-top" |"left-bottom" |"Center" | "center-top"
+ * "center-bottom" | "Right-center" | "Right-Top" | "Right-Bottom"
+ */
+
+export const Toast=({Position})=>{
+    return (
+        <div>Toast Notification Position - {Position}</div>
+    )
+}
