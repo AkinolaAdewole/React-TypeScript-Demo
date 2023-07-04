7db4829c3aa1019283241d1d1b663445af8cb09d
Akinola <adewoleakinola24@gmail.com>
2023-07-04 17:26:29 +0100

    TEMPLATE LITERALS

--- src/components/Status.tsx
+++ src/components/Status.tsx
@@ -1,6 +1,15 @@
 import { type } from 'os'
 import React from 'react'
 
+
+// In React TypeScript, string literals can be used to define specific values or types by creating a union type with literal values. 
+// This is useful when you want to restrict a variable or prop to accept only a specific set of string values.
+//  Here's an example of using string literals in React 
+
+// Using string literals with union types in React TypeScript helps enforce type safety and prevents unintended values from being passed to components.
+//  It provides clear and specific options for the acceptable values of certain variables or props.
+
+
 type statusProps={
     status:'Loading'| 'Success' | 'Error'
 }
