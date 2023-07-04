8a30fb664371c6b6f0fd0127f06f7dab8ccb86cc
Akinola <adewoleakinola24@gmail.com>
2023-07-04 10:34:27 +0100

    DOM Ref and Mutable Ref

--- src/components/ref/DomRef.tsx
+++ src/components/ref/DomRef.tsx
@@ -4,6 +4,13 @@ import React,{useRef,useEffect} from 'react'
 // Side effects can include fetching data, subscribing to events, 
 // manipulating the DOM, or any other operation that affects the outside world.
 
+// DOM Refs (Ref forwarding):
+// DOM refs, also known as "ref forwarding," are primarily used to reference and interact with DOM elements directly. 
+// They are created using the createRef() function or the useRef() hook. DOM refs allow you to access properties and methods of DOM elements, 
+// such as inputRef.current.value or inputRef.current.focus(). 
+// DOM refs are typically used when you need direct access to a specific DOM element or to integrate with third-party libraries that require DOM manipulation
+
+
 const DomRef = () => {
   // since I am accessing input, I will use HTMLinputElement and add exclamation mark to null
     const inputRef=useRef<HTMLInputElement>(null!)
--- src/components/ref/MutableRef.tsx
+++ src/components/ref/MutableRef.tsx
@@ -3,11 +3,31 @@
 // manipulating the DOM, or any other operation that affects the outside world.
 
 
+// Mutable Refs:
+// Mutable refs, created using the useRef() hook, are more general-purpose and can be used to store any mutable value or reference. 
+// Mutable refs are not limited to DOM elements and can hold any JavaScript value, including objects, arrays, or other mutable values. 
+// They persist across component renders and can be updated without causing a re-render of the component.
+
+
 import React,{useState,useRef, useEffect} from 'react'
 
 const MutableRef = () => {
-    const [timer, setTimer]=useState(null)
-    const intervalRef=useRef(null)
+    const [timer, setTimer]=useState(0)
+    const intervalRef=useRef<number | undefined>(undefined)
+
+    const stopTimer=()=>{
+        window.clearInterval(intervalRef.current);
+    }
+
+    useEffect(()=>{
+        intervalRef.current= window.setInterval(()=>{
+            setTimer((timer)=>timer+1)
+        },1000)
+       
+        return ()=>{
+            stopTimer()
+        }
+    },[])
 
   return (
     <div>MutableRef</div>
