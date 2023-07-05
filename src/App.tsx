7d95805e48f2ac024d24b3d28160e8aae3fa50fd
Akinola <adewoleakinola24@gmail.com>
2023-07-05 08:32:43 +0100

    Polymorphic component

--- src/App.tsx
+++ src/App.tsx
@@ -19,6 +19,7 @@ import { Profile } from "./components/auth/Profile";
 import { List } from "./components/generics/List";
 import { RandomNumber } from "./components/restriction/RandomNumber";
 import Toast from "./components/TemplateLiterals/Toast";
+import CustomButton from "./components/html/Button";
 
 function App() {
   const personName = {
@@ -94,7 +95,11 @@ function App() {
 
         <Toast position="Left-Center" />
 
-        <B
+
+        <CustomButton variant="Primary" onClick={()=>console.log('clicked')}>
+          Primary Button
+        </CustomButton>
+
     </div>
   );
 }
--- src/components/html/Input.tsx
+++ src/components/html/Input.tsx
@@ -1,1 +1,14 @@
-export const Input=()=>{}
+import React from 'react'
+
+type InputProps=React.ComponentProps<'input'>
+
+const CustomInput = (Props:InputProps) => {
+
+  return (
+    <div>
+        <input type="text" {...Props} />
+    </div>
+  )
+}
+
+export default CustomInput
